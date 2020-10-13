3dc237c8914090a6ca5e0c4be5e5944380bb9022
Giant Waffle <waffle@giantwaffle.tv>
2020-10-12 22:02:11 -0500

    Fibonacci

---
+++ Even Fibonacci Numbers.py
@@ -0,0 +1,14 @@
+current_digit = 1
+last_digit = 1
+new_digit = 1
+total = 0
+
+while current_digit <= 4000000:
+    print(current_digit)
+    if (current_digit % 2) == 0:
+        total = total + current_digit
+    new_digit = current_digit + last_digit  # This part can be a lot better
+    last_digit = current_digit
+    current_digit = new_digit
+
+print("Total", total)
